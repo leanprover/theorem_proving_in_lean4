d0e4ef7de3fd927d1fd9c48ac3adc0197509de8e
Soonho Kong <soonhok@cs.cmu.edu>
2014-10-31 17:31:19 -0700

    chore(elisp/org-batch.el): clean up

--- elisp/org-batch.el
+++ elisp/org-batch.el
@@ -3,23 +3,15 @@
 (cask-initialize "./")
 (setq working-dir (f-dirname (f-this-file)))
 (add-to-list 'load-path working-dir)
+(setq make-backup-files nil)
 
 ;;; ORG mode
 (require 'org)
-;; (require 'ob-lean)
-;; (org-babel-do-load-languages 'org-babel-load-languages '((lean . t)))
-
-;; Lean mode
-(setq lean-rootdir "lean")
-(customize-set-variable 'lean-flycheck-use nil)
-(customize-set-variable 'lean-follow-changes nil)
-(require 'lean-mode)
 
 ;; set auto load on .org files
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 
 ;; org mode customisations
-(setq org-export-htmlize-output-type 'css)
 '(org-export-blocks (quote ((comment org-export-blocks-format-comment t) (ditaa org-export-blocks-format-ditaa nil) (dot org-export-blocks-format-dot t) (r org-export-blocks-format-R nil) (R org-export-blocks-format-R nil))))
 '(org-export-html-inline-images t)
 '(org-export-html-use-infojs t)
