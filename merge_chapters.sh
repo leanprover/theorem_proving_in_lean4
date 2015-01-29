90f55040e5a4dd0b9e2c67b71a7840ffde17eac0
Soonho Kong <soonhok@cs.cmu.edu>
2015-01-29 14:02:35 -0500

    feat(merge_chapters.sh): resolve inter-file links by replacing them with inner-file links
    Close #44

--- merge_chapters.sh
+++ merge_chapters.sh
@@ -21,3 +21,15 @@ do
         tail -n +${START_LINE} -- ${CHAPTER} >> ${TUTORIAL_ORG_FILE}
     fi
 done
+
+# Replace inter-file links to inner-file links
+#
+# Example:
+#
+#     In Section [[file:05_Interacting_with_Lean.org::Notation_Overloads_and_Coercions][Notations, Overloads, and Coercions]], we discussed coercions
+#
+# =>
+#
+#     In Section [[Notation_Overloads_and_Coercions][Notations, Overloads, and Coercions]], we discussed coercions
+sed -e "s/file:[0-9][0-9]_[^:]*.org:://g" ${TUTORIAL_ORG_FILE} > ${TUTORIAL_ORG_FILE}.tmp
+mv -v ${TUTORIAL_ORG_FILE}.tmp  ${TUTORIAL_ORG_FILE}
