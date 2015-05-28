d4ecd36b400e5e3df908365a6e1680902598880d
Soonho Kong <soonhok@cs.cmu.edu>
2015-05-28 07:58:40 -0400

    feat: add index
    close #107

--- elisp/org-pdf-export.el
+++ elisp/org-pdf-export.el
@@ -11,8 +11,26 @@
 
 ;;; XeLaTeX customisations
 ;; remove "inputenc" from default packages as it clashes with xelatex
-(setf org-latex-default-packages-alist
-      (remove '("AUTO" "inputenc" t) org-latex-default-packages-alist))
+;; (setf org-latex-default-packages-alist
+;;       (remove '("AUTO" "inputenc" t) org-latex-default-packages-alist))
+
+(setq org-latex-default-packages-alist
+      '(("T1" "fontenc" t)
+        ("" "fixltx2e" nil)
+        ("" "graphicx" t)
+        ("" "longtable" nil)
+        ("" "float" nil)
+        ("" "wrapfig" nil)
+        ("" "rotating" nil)
+        ("normalem" "ulem" t)
+        ("" "amsmath" t)
+        ("" "textcomp" t)
+        ("" "marvosym" t)
+        ("" "wasysym" t)
+        ("" "amssymb" t)
+        ("" "imakeidx" nil)
+        ("" "hyperref" nil)
+        "\\tolerance=1000"))
 
 (setq org-latex-listings 'minted)
 (setq org-export-with-smart-quotes t)
