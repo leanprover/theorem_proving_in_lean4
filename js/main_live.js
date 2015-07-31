41f187358ec475ef6120582086b4316960100738
Soonho Kong <soonhok@cs.cmu.edu>
2015-07-30 18:09:09 -0700

    feat(js/main_live.js): show console and main window when running code

--- js/main_live.js
+++ js/main_live.js
@@ -548,6 +548,12 @@ var myModule = (function() {
             FS.writeFile(filename, text, {encoding: 'utf8'});
         },
         process_main_buffer: function() {
+            if (myModule.get_main_console_ratio() == 1.0) {
+                myModule.set_main_console_ratio(0.8);
+            }
+            if (myModule.get_tutorial_main_ratio_ratio() == 1.0) {
+                myModule.set_tutorial_main_ratio(0.5);
+            }
             this.clear_console();
             myModule.append_console_nl("-- Processing...");
             var start_time = new Date().getTime();
