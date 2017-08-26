623b3b6848204adad6c7beaee34e56ac9bb4b7e2
Gabriel Ebner <gebner@gebner.org>
2017-08-26 09:53:45 +0200

    chore(lean_sphinx): new url for the 3.2.0 live version

--- lean_sphinx.py
+++ lean_sphinx.py
@@ -21,7 +21,7 @@ except:
 class lean_code_goodies(nodes.General, nodes.Element): pass
 
 def mk_try_it_uri(code):
-    uri = 'https://gebner.github.io/lean-web-editor/#code='
+    uri = 'https://leanprover.github.io/live/3.2.0/#code='
     uri += urlquote(code, safe='~@#$&()*!+=:;,.?/\'')
     return uri
 
