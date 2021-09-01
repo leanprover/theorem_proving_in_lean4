15cb7513b2af65c840118e93215061c595cd678a
Leonardo de Moura <leonardo@microsoft.com>
2021-09-01 10:28:20 -0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 set -e
-if [ "$#" -ne 2 ]; then
-    echo "Usage example: $0 leanprover theorem_proving_in_lean"
-    exit 1
-fi
 
 # Build
-make clean html latexpdf
+mdbook build
+rm -rf out/.git
 
 # 3. Deploy
 rm -rf deploy
 mkdir deploy
 cd deploy
 git init
-cp -r ../_build/html/./ .
-cp ../_build/latex/theorem_proving_in_lean.pdf .
-git add .
+cp -r ../out/./ .
+git add -A
 git commit -m "Update `date`"
-git push git@github.com:$1/$2 +HEAD:gh-pages
-cd ../
+git push https://github.com/leanprover/theorem_proving_in_lean4.git +HEAD:gh-pages
+cd ..
 rm -rf deploy
