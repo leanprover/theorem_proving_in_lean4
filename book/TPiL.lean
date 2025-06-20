4dcbcc91baef089ead11a94f8915d192c354ac13
David Thrane Christiansen <david@davidchristiansen.dk>
2025-06-20 12:36:26 +0200

    chore: show version string on front page

--- book/TPiL.lean
+++ book/TPiL.lean
@@ -15,6 +15,17 @@ import TPiL.AxiomsComputation
 open Verso.Genre Manual
 open Verso Code External
 
+open Verso Doc Elab in
+open Lean (quote) in
+@[role_expander versionString]
+def versionString : RoleExpander
+  | #[], #[] => do
+    let version ← IO.FS.readFile "../examples/lean-toolchain"
+    let version := version.stripPrefix "leanprover/lean4:" |>.trim
+    pure #[← ``(Verso.Doc.Inline.code $(quote version))]
+  | _, _ => throwError "Unexpected arguments"
+
+
 #doc (Manual) "Theorem Proving in Lean 4" =>
 
 %%%
@@ -22,7 +33,7 @@ authors := ["Jeremy Avigad", "Leonardo de Moura", "Soonho Kong", "Sebastian Ullr
 %%%
 
 
-This version of the text assumes you’re using Lean 4. See the
+This version of the text assumes you’re using Lean 4 (specifically {versionString}[]). See the
 [Quickstart section](https://lean-lang.org/documentation/setup/) of
 the Lean documentation to install Lean. The first version of this book was
 written for Lean 2, and the Lean 3 version is available
